06b3bb6d338e44b7e77385506fdef502c79f21e0
Lucas Rodrigues <ludecaro@outlook.com>
2023-12-27 16:51:47 -0300

    update security group

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -1,45 +1,22 @@
-resource "aws_security_group" "alb" {
-  name        = "alb_ECS"
+resource "aws_security_group" "ssh_cluster" {
+  name        = "ssh_cluster"
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "tcp_alb" {
+resource "aws_security_group_rule" "ssh_cluster_in" {
   type              = "ingress"
-  from_port         = 8000
-  to_port           = 8000
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.ssh_cluster.id
 }
 
-resource "aws_security_group_rule" "saida_alb" {
+resource "aws_security_group_rule" "ssh_cluster_out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.alb.id
-}
-
-resource "aws_security_group" "privado" {
-  name        = "privado_ECS"
-  vpc_id      = module.vpc.vpc_id
-}
-
-resource "aws_security_group_rule" "entrada_ECS" {
-  type              = "ingress"
-  from_port         = 0 
-  to_port           = 0
-  protocol          = "-1" #Permite qualquer protocolo
-  source_security_group_id = aws_security_group.alb.id
-  security_group_id = aws_security_group.privado.id
-}
-
-resource "aws_security_group_rule" "saida_ECS" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1" #Permite qualquer protocolo
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
-  security_group_id = aws_security_group.privado.id
+  security_group_id = aws_security_group.ssh_cluster.id
 }
