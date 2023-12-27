ba2081d074a90863a2c6c940e31d8158a43ca16e
Lucas Rodrigues <ludecaro@outlook.com>
2023-12-27 16:51:35 -0300

    update provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,3 +13,17 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
+
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
