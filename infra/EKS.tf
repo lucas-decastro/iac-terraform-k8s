6e3fdb264ff8178589ff92706d3406ad06a91123
Lucas Rodrigues <ludecaro@outlook.com>
2023-12-27 16:51:08 -0300

    subindo eks blue e green

---
+++ infra/EKS.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                    = var.cluster_name
+  cluster_version                 = "1.28"
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    #blue = {
+      #min_size     = 1
+      #max_size     = 10
+      #desired_size = 3
+      #vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+      #instance_types = ["t2.micro"]
+    #}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+      vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+      instance_types = ["t2.micro"]
+    }
+  }
+}
